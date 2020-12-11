bd757af9c60aa7788a17629f4023dc254ea8b9c2
Celso Bertolotto <celsonyl@hotmail.com>
2020-12-11 15:14:22 -0300

    sextou div create no label ok

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -20,5 +20,10 @@ namespace SalesWebMvc.Controllers
             var list = _sellerService.FindAll(); 
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
