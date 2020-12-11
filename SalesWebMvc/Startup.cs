db29a025cc38f4f440c14b333a65e833846bfbb5
Celso Bertolotto <celsonyl@hotmail.com>
2020-12-11 14:58:49 -0300

    Seller service e basic findAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
-
+using SalesWebMvc.Services;
 namespace SalesWebMvc
 {
     public class Startup
@@ -41,6 +41,7 @@ namespace SalesWebMvc
                     builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
