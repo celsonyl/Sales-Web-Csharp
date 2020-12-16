82f2c7a68482b2c051d4585f19b85c00b9709f96
Celso Bertolotto <celsonyl@hotmail.com>
2020-12-16 15:04:24 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,6 +21,18 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList(); 
         }
 
+        public Seller FindById(int id)
+        {
+
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
         public void Insert(Seller obj)
         {
             _context.Add(obj);
