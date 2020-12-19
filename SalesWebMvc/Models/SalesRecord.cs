65ed3a33c69f704c9f05286c81077907b162ae1b
Celso Bertolotto <celsonyl@hotmail.com>
2020-12-19 10:06:50 -0300

    colocando sistema de busca em grupo

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,13 +1,15 @@
 ﻿using SalesWebMvc.Models.Enums;
 using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public Double Amount { get; set; }
         public SalesStatus Status { get; set; }
         public Seller Seller { get; set; }
